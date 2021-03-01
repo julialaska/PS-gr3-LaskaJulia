8373f448d8a681d7f6fd060b0e3c8e46eada5c98
laskajulia <julkalas101@gmail.com>
2021-03-01 13:40:26 +0100

    lab 0 cd

---
+++ lab0/1.2/z124.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{   int liczba;
+    scanf("%d", &liczba);
+    printf("%d", liczba);
+    return 0;
+}
---
+++ lab0/1.2/z126.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{   int liczba1, liczba2, liczba3;
+    scanf("%d" "%d" "%d", &liczba1, &liczba2, &liczba3);
+    printf("%d\n" "%d\n" "%d\n", liczba1, liczba2, liczba3);
+    return 0;
+}
