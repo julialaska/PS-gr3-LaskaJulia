bfb8487c0e43c0d1d86340e6d4c53a5af5ee453e
laska.julia <julkalas101@gmail.com>
2021-04-12 13:55:34 +0200

    koniec

--- Kolokwium1-LaskaJulia/zad_nr1.c
+++ Kolokwium1-LaskaJulia/zad_nr1.c
@@ -9,26 +9,26 @@ int main()
 
     int i;
     int j;
+    int suma_pierw=0;
     for(i=1;i<=n;i++)
     {
-        if(n%i == 0)
-            printf("%d\n",i);
-
+        if(n%i == 0)//dzielniki
+            {
+            for(j=2;j<i;j++)
+            {
+                if((i%j==0))
+                    printf(" nie jest pierwsza %d\n", i);
+                suma_pierw++;
+            }
+            }
+        else
+            printf("nie jest dzielnikiem");
     }
 
-    if(i<2)
-        printf("mniejsze niz 2 nie sa pierwsze");
-    else
-        for(j=1;j*j<=i;j++)
-        {
-            if((i%j==0))
-               printf("%d nie jest pierwsza", j);
-            printf("%d\n",i);
-
-        }
 
 
 
+printf("%d",suma_pierw);
 
 
 
