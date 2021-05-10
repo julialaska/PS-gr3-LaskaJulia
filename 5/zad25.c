fa4329af89fcf22f06d1141b3bf66e909d2a782a
julialaska <73698292+julialaska@users.noreply.github.com>
2021-05-10 14:32:54 +0200

    Add files via upload

---
+++ 5/zad25.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+char kopiuj(char *nap1, char**nap1)
+{
+    char tab[];
+    for(int i=0;i<sizeof(nap1);i++)
+    {
+        tab[i] = nap1[i];
+    }
+    tab=**nap1
+
+}
---
+++ 5/zad6.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+char *kopiujn(const char*nap, char*nap2, int n)
+{
+    for(int i=0; i < n; i++)
+    {
+        nap2[i] = nap[i];
+    }
+    return nap2;
+}
+
+int main()
+{
+    char nap[]="anakonda";
+    char nap2[365]="boa";
+    int n;
+    printf("podaj n ");
+    scanf("%d",&n);
+
+    printf("%s\n",nap2);
+
+
+
+    printf("%s\n",kopiujn(nap,nap2,n));
+    return 0;
+}
